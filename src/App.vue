a731979e527f660728f363d001360c78660f0605
GabriellyDiasPereira <gabriellydiaspereira15@gmail.com>
2023-05-01 14:27:35 -0300

    criando formulário

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,67 @@
 <script setup>
-import { ref, onMounted } from 'vue'
+import { reactive, onMounted } from "vue";
 
-// reactive state
-const count = ref(0)
+let filme = reactive({
+  nome: "",
+  capa: "",
+  assistido: false,
+});
 
 // functions that mutate state and trigger updates
 function increment() {
-  count.value++
+  count.value++;
 }
-
 </script>
 
 <template>
-  <button @click="increment">Count is: {{ count }}</button>
+  <div class="container">
+    <h1>Minha lista de filmes</h1>
+    <form class="formulario">
+      <div class="entrada">
+        <label> nome: </label>
+        <input v-model="filme.nome" type="text" name="nome" />
+      </div>
+      <div class="entrada">
+        <label> capa: </label>
+        <input v-model="filme.capa" type="text" name="capa" />
+      </div>
+      <div class="checkbox-entrada">
+        <label> assistido: </label>
+        <input
+          v-model="filme.assistido"
+          class="assistindo"
+          type="checkbox"
+          name="assistido"
+        />
+      </div>
+      <button>registrar</button>
+    </form>
+    {{ JSON.stringify(filme) }}
+  </div>
 </template>
+
+<style>
+.container {
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+}
+.formulario {
+  flex: 1;
+  flex-direction: column;
+  align-items: center;
+  padding-bottom: 30px;
+}
+.entrada {
+  margin-bottom: 10px;
+  display: flex;
+  flex-direction: column;
+}
+.checkbox-entrada {
+  display: flex;
+  margin-bottom: 10px;
+}
+.assistindo {
+  margin-left: 10px;
+}
+</style>
